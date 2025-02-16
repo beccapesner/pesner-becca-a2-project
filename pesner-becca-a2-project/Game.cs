f721f8a5e9dffaff5456f2457554f0a3b40d7398
Becca Pesner <000351580@mohawkcollege.ca>
2025-02-15 23:21:21 -0500

    add base drawing

--- pesner-becca-a2-project/Game.cs
+++ pesner-becca-a2-project/Game.cs
@@ -3,31 +3,86 @@ using System;
 using System.Numerics;
 
 // The namespace your code is in.
-namespace MohawkGame2D
+namespace MohawkGame2D;
+
+/// <summary>
+///     Your game code goes inside this class!
+/// </summary>
+public class Game
 {
+    // Place your variables here:\
+    Color almostBlack = new Color("030206");
+    Color deepestBlue = new Color("070918");
+    Color deepBlue = new Color("0f052d");
+    Color navy = new Color("203671");
+    Color babyBlue = new Color("36b5f5");
+    Color neonBlue = new Color("3afffb");
+    Color teal = new Color("36868f");
+    Color green = new Color("5fc75d");
+    Color neonGreen = new Color("83c16f");
+    int starCount = 40;
+    int[] starPositionsX;
+    int[] starPositionsY;
+
+
     /// <summary>
-    ///     Your game code goes inside this class!
+    ///     Setup runs once before the game loop begins.
     /// </summary>
-    public class Game
+    public void Setup()
     {
-        // Place your variables here:
+        Window.SetTitle("Fishing");
+        Window.SetSize(400, 400);
+        Draw.LineSize = 0;
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
-        public void Setup()
+        // Generate random star positions
+        // First, initialize the array to contain a certain number of elements
+        starPositionsX = new int[starCount];
+        starPositionsY = new int[starCount];
+        // Next, go through array and assign each index a value
+        for (int i = 0; i < starCount; i++)
         {
-
-        }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
-        public void Update()
-        {
-
+            starPositionsX[i] = Random.Integer(0, 400); // x = 0-400
+            starPositionsY[i] = Random.Integer(0, 300); // y = 0-300
         }
     }
 
+    /// <summary>
+    ///     Update runs every frame.
+    /// </summary>
+    public void Update()
+    {
+        //Draw sky
+        Window.ClearBackground(almostBlack);
+
+        //Draw stars
+        Draw.FillColor = neonBlue;
+        for (int i = 0; i < starCount; i++)
+        {
+            Draw.Circle(starPositionsX[i], starPositionsY[i], 2);
+        }
+
+        //Draw background waves
+        Draw.FillColor = navy;
+        for (int i = 0; i < 16; i++)
+        {
+            int x = 12 + i * 25;
+            Draw.Circle(x, Window.Height - 100, 19);
+        }
+
+        //Draw midground waves
+        Draw.FillColor = teal;
+        for (int i = 0; i < 8; i++)
+        {
+            int x = 25 + i * 50;
+            Draw.Circle(x, Window.Height - 65, 38);
+        }
+
+        //Draw foreground waves
+        Draw.FillColor = deepBlue;
+        for (int i = 0; i < 4; i++)
+        {
+            int x = 50 + i * 100;
+            Draw.Circle(x, Window.Height, 75);
+        }
+    }
 }
